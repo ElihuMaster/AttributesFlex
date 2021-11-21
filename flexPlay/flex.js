f7b7d858994520429fdebbb2fe8e42605ff6d8e3
ElihuMaster <yopiscis_852@hotmail.com>
2021-11-21 11:00:07 -0500

    atributos-flex

--- flexPlay/flex.js
+++ flexPlay/flex.js
@@ -1,6 +1,16 @@
 
 // button functionality >> add
 const container = document.querySelector('.container-padre')
+// numbersGap = []
+// function gap (){
+//     valorGAp = document.querySelector('.input-gap').value
+//     if (valorGAp > 0) {
+//         numbersGap = valorGAp.value
+//     }
+
+//     return valorGAp
+// } 
+// console.log(gap());
 
 let colorHex = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
 let numeros = []
